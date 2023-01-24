4af0a9142b204b0388d1526deb525faae4f20f75
Alex <alexlagwick12@gmail.com>
2023-01-24 13:50:52 +0100

    init: opload solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let result = [];
+    for (let i = 1; i < matrix.length; i = i + 2) {
+      matrix[i].reverse();
+    }
+    result = [].concat(...matrix);
+    return result;
+  };
+
